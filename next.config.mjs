94ad1d9020ab9d6118f3ec88cdca842cb9187fa8
YasserAbdalla <yasserabdalla149@gmail.com>
2024-07-02 22:40:19 +0300

    Open the cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // Enable CORS on all API routes
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-Requested-With, Authorization, Content-Type, Accept, Origin",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
